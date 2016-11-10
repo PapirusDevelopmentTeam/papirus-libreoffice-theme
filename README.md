9c60c54668e257159c3664f580f2f2095233b06e
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-10 18:36:43 +0300

    omg

---
+++ README.md
@@ -0,0 +1,12 @@
+<p align="center">
+  <img src="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/preview.png" alt="preview"/>
+</p>
+
+# Install / Update
+```
+wget -O - https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/install-papirus.sh | bash
+```
+**Depends:**
+- wget
+- unzip
+- libreoffice
---
+++ install-papirus.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+echo "Papirus LibreOffice themes"
+sleep 5
+echo "Delete old Papirus icon theme ..."
+sudo rm /usr/share/libreoffice/share/config/images_papiru*
+echo "Download new version from GitHub ..."
+sudo wget -c https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/images_papirus.zip -O /usr/share/libreoffice/share/config/images_papirus.zip
+sudo wget -c https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/images_papirus_dark.zip -O /usr/share/libreoffice/share/config/images_papirus_dark.zip
+echo "Installing ..."
+sudo ln -s /usr/share/libreoffice/share/config/images_papirus.zip /usr/lib/libreoffice/share/config/images_papirus.zip
+sudo ln -s /usr/share/libreoffice/share/config/images_papirus_dark.zip /usr/lib/libreoffice/share/config/images_papirus_dark.zip
+sudo chmod -R 755 /usr/share/libreoffice/share/config/images_papiru*
+echo "Delete cache ..."
+rm install-papirus.sh
+echo "Done!"
