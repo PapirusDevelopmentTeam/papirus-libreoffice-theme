6b686d9cb54282abd46caaeebb1be0c044253a26
Sergei Eremenko <finalitik@gmail.com>
2016-11-26 20:49:15 +0200

    New installer

--- install-papirus-root.sh
+++ install-papirus-root.sh
@@ -1,13 +1,48 @@
-#!/usr/bin/env bash
-echo "Papirus LibreOffice themes"
-sleep 5
-echo "Delete old Papirus icon theme ..."
-sudo rm /usr/share/libreoffice/share/config/{images_papirus.zip,images_papirus_dark.zip}
-echo "Download new version from GitHub ..."
-sudo wget -c https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/images_papirus.zip -O /usr/share/libreoffice/share/config/images_papirus.zip
-sudo wget -c https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/images_papirus_dark.zip -O /usr/share/libreoffice/share/config/images_papirus_dark.zip
-echo "Installing ..."
-sudo ln -s /usr/share/libreoffice/share/config/images_papirus.zip /usr/lib/libreoffice/share/config/images_papirus.zip
-sudo ln -s /usr/share/libreoffice/share/config/images_papirus_dark.zip /usr/lib/libreoffice/share/config/images_papirus_dark.zip
-sudo chmod -R 755 /usr/share/libreoffice/share/config/images_papiru*
-echo "Done!"
+#!/bin/sh
+
+set -e
+
+gh_repo="papirus-libreoffice-theme"
+gh_desc="Papirus LibreOffice themes"
+
+cat <<- EOF
+
+
+
+      ppppp                         ii
+      pp   pp     aaaaa   ppppp          rr  rrr   uu   uu     sssss
+      ppppp     aa   aa   pp   pp   ii   rrrr      uu   uu   ssss
+      pp        aa   aa   pp   pp   ii   rr        uu   uu      ssss
+      pp          aaaaa   ppppp     ii   rr          uuuuu   sssss
+                          pp
+                          pp
+
+
+  $gh_desc
+  https://github.com/PapirusDevelopmentTeam/$gh_repo
+
+
+EOF
+
+temp_dir=$(mktemp -d)
+
+echo "=> Getting the latest version from GitHub ..."
+wget -O "/tmp/$gh_repo.tar.gz" \
+  https://github.com/PapirusDevelopmentTeam/$gh_repo/archive/master.tar.gz
+echo "=> Unpacking archive ..."
+tar -xzf "/tmp/$gh_repo.tar.gz" -C "$temp_dir"
+echo "=> Deleting old $gh_desc ..."
+sudo rm -rf "/usr/share/libreoffice/share/config/images_papirus.zip" \
+  "/usr/share/libreoffice/share/config/images_papirus_dark.zip"
+echo "=> Installing ..."
+sudo cp --no-preserve=mode,ownership -r \
+  "$temp_dir/$gh_repo-master/images_papirus.zip" \
+  "$temp_dir/$gh_repo-master/images_papirus_dark.zip" \
+  "/usr/share/libreoffice/share/config/"
+sudo ln -s /usr/share/libreoffice/share/config/images_papirus.zip \
+  /usr/lib/libreoffice/share/config/images_papirus.zip
+sudo ln -s /usr/share/libreoffice/share/config/images_papirus_dark.zip \
+  /usr/lib/libreoffice/share/config/images_papirus_dark.zip
+echo "=> Clearing cache ..."
+rm -rf "/tmp/$gh_repo.tar.gz" "$temp_dir"
+echo "=> Done!"
